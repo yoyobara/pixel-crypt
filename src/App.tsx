c5685d7057428c468370c144e47f857cc336ea7a
yoyobara <yotambarak6@gmail.com>
2025-01-25 20:26:19 +0200

    Fix(*): starting over with MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import Navbar from "./components/navbar/Navbar"
-import "./global.css"
-
 function App() {
     return (
         <div className="app">
-            <Navbar/>
         </div>
     )
 }
